19ea3260592a06fae8e2968dd9d00a03244c4be8
Adam <adam@coderdojo.org>
2016-06-24 22:44:46 +0100

    Run script every two hours

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var request = require('request');
 var nodemailer = require('nodemailer');
 var fs = require('fs');
+var CronJob = require('cron').CronJob;
 var data = require('./data.json');
-
 
 var transporter = nodemailer.createTransport({
   service: 'Gmail',
@@ -21,29 +21,31 @@ console.log('SMTP Configured');
 var key = 'API_KEY_HERE';
 var endPoint = "https://www.googleapis.com/qpxExpress/v1/trips/search?key="+key;
 
-request({method: "post",  url: endPoint,  body: data,  json: true}, function(err, resp, body){
-  if (body.error){
-    return console.error(body.error);
-  } else {
-    var price= body.trips.tripOption[0].saleTotal;
-    var message = {
-      to: '"NAME_HERE" <RECEIVING_EMAIL_ADDRESS_HERE>',
-      subject: 'Price update',
-      text: ''+price+''
-    };
+new CronJob('0 0 */2 * * *', function (){
+  request({method: "post",  url: endPoint,  body: data,  json: true}, function(err, resp, body){
+    if (body.error){
+      return console.error(body.error);
+    } else {
+      var price= body.trips.tripOption[0].saleTotal;
+      var message = {
+        to: '"NAME_HERE" <RECEIVING_EMAIL_ADDRESS_HERE>',
+        subject: 'Price update',
+        text: ''+price+''
+      };
 
-    console.log('Sending mail');
-    transporter.sendMail(message, function(error, info){
-      if(error){
-        console.log(error.message);
-        return;
-      }
-      console.log('Email sent!');
-      price = price.replace('EUR','');
-      savePrice('\n'+price);
-    })
-  }
-});
+      console.log('Sending mail');
+      transporter.sendMail(message, function(error, info){
+        if(error){
+          console.log(error.message);
+          return;
+        }
+        console.log('Email sent!');
+        price = price.replace('EUR','');
+        savePrice('\n'+price);
+      })
+    }
+  });
+}, null, true, 'Europe/Dublin');
 
 function savePrice(price){
   fs.appendFile('prices.txt', price, function(err){
